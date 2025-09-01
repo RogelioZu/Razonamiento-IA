3fc9d351d6dd750c17b91d778f22823b7b7a8f8a
albertovaldezreyes <albertovaldezreyes99@gmail.com>
2025-09-01 15:11:06 -0600

    introducir gui

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+from tkinter import *
 import HaciaAdelante as A
 import HaciaAtras as AS
 
+window = Tk()
+label = Label(window, text = "Escriba 2 elementos para la base de conocimiento: ")
+textField1 = Text(height=1, width=20)
+textField2 = Text(height=1, width=20)
+textField3 = Text(height=1, width=20)
+button = Button(window, text = "procesar")
+label.pack()
+textField1.pack()
+textField2.pack()
+textField3.pack()
+button.pack()
 
-print("Escriba 2 elemntos para la base de conocimiento: ")
+window.mainloop()
+
 bc = []
 
 
---
+++ main_headless.py
@@ -0,0 +1,27 @@
+import HaciaAdelante as A
+import HaciaAtras as AS
+
+
+print("Escriba 2 elemntos para la base de conocimiento: ")
+bc = []
+
+
+bc.append(input())
+bc.append(input())
+
+print("Introduzca la meta: ")
+meta = input()
+
+print(f"Base de conocimiento {bc}")
+print("meta: " + meta)
+
+
+
+#adelante = A.HaciaAdelante(meta,bc)
+#adelante.proceso()
+
+
+
+
+atras = AS.HaciaAtras(meta,bc)
+atras.proceso()
